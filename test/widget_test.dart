8f8551c36c4901848a67fb16afc07b002edb3c52
Faizan Sami <faizansami6859@gmail.com>
2026-06-18 16:02:00 +0500

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:volume_booster_fresh/main.dart';
+import 'package:volume_booster_fresh/views/widgets/metallic_knob.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
